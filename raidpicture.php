747085e57d28248d575d025d9e7c04fd61c0c248
Ossi Kuisma <ossikuisma@hotmail.com>
2020-08-12 20:29:18 +0300

    Finalizing the new pokemon table stuff

--- raidpicture.php
+++ raidpicture.php
@@ -189,7 +189,7 @@ if($time_now < $raid['end_time']) {
         $pokemon_id = str_pad($raid['pokedex_id'], 3, '0', STR_PAD_LEFT);
 
         // Getting the actual icon
-        if($raid['shiny'] == 1) {
+        if($raid['shiny'] == 1 && !$config->RAID_PICTURE_DISABLE_SHINY) {
         	$img_pokemon = imagecreatefrompng(IMAGES_PATH . "/pokemon/pokemon_icon_" . $icon_suffix . "_shiny.png");    
         } else {
         	$img_pokemon = imagecreatefrompng(IMAGES_PATH . "/pokemon/pokemon_icon_" . $icon_suffix . ".png");
